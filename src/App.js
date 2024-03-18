27799cc4fd8590a5506c18bd9fc4d447bee3e281
AyeshaShamail <shamail.ayesha.02@gmail.com>
2024-03-18 14:41:07 +0530

    netflix 2nd commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="text-3xl font-bold text-red-400">
-      Salam Everyone, let's build Netflix-GPT
+      Hi Everyone, let's build Netflix-GPT
     </div>
   );
 }
