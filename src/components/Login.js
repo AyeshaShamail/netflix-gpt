779875b118515fd76454947beba0e8ccc5061497
AyeshaShamail <shamail.ayesha.02@gmail.com>
2024-03-28 11:54:37 +0530

    Removing console logs

--- src/components/Login.js
+++ src/components/Login.js
@@ -60,7 +60,6 @@ const Login = () => {
               // An error occurred
               setErrorMessage(error.message);
             });
-          // console.log(user);
         })
         .catch((error) => {
           const errorCode = error.code;
